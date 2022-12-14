21c60983fceef4e2cb0b2ba22b7458e0f98c8713
Seonu <seonup95@gmail.com>
2022-12-14 17:08:44 +0900

    feat: Create Component class
    - Component 호출시 props를 인자로 받고, holdEvents 메서드 호출
    - setState 메서드 생성
    - holdEvents 메서드 생성
    - render 메서드 생성

--- front/src/core/Component.js
+++ front/src/core/Component.js
@@ -1,1 +1,54 @@
-// 모든 컴포넌트가 가지고 있을 Component.js
+import { render, eventHolder } from '../dom/index';
+
+class Component {
+  constructor(props) {
+    this.props = props;
+    this.#holdEvents();
+  }
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    render();
+  }
+
+  /**
+   * @private
+   * Component의 constructor가 호출될 때만 holdEvents 메서드가 호출되면 되므로, 자식 class에게 상속하지 않는다.
+   */
+  #holdEvents() {
+    // 이벤트가 중복 등록되지 않도록 체크하는 holdEvents 메서드 생성
+    const events = this.addEventListener?.();
+    if (!events) return;
+
+    for (const event of events) {
+      // eventHolder에 events와 동일한 내용이 있으면 추가하지 않는다.
+      const duplication = eventHolder.find(
+        ({ type, selector, handler }) => type === event.type && selector === event.selector && handler === event.handler
+      );
+
+      if (!duplication) {
+        const { selector, handler } = event;
+        /**
+         * 이벤트 핸들러 실행조건 추가.
+         * 이벤트 target이 selector 자신이거나 하위 요소인 경우 함수를 호출한다.
+         */
+        event.handler = e => {
+          if (e.target.closest(selector)) handler(e);
+        };
+
+        eventHolder.push(event);
+      }
+    }
+  }
+
+  /**
+   * @abstract
+   * 자식 컴포넌트가 render 메서드를 가지고 있지 않다는 에러를 띄우기 위한 목적으로 사용된다.
+   * 컴포넌트들을 props 방식으로 만들고 있기 때문에, 자식 컴포넌트들은 모두 render 메서드를 가져야 한다.
+   */
+  render() {
+    throw new Error('컴포넌트가 render 메서드를 가지고 있지 않습니다. 생성해주세요.');
+  }
+}
+
+export default Component;
