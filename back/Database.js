ec62c97a232d6722ad68f2ebb4e29073c79cd814
NamgungJongMin <jmnamgung@gmail.com>
2022-12-14 14:48:27 +0900

    feat: Create fake-data about users information
    - Database 파일 생성
    - 유저 정보만을 객체로 저장 (각 프로퍼티 키에 대한 접근은 server 구현
      진행하면서 리팩토링할 예정)

---
+++ back/Database.js
@@ -0,0 +1,28 @@
+const usersInfo = [
+  {
+    userId: 1,
+    email: 'dumdum1@naver.com',
+    name: '코카콜라맛있다',
+    password: '111111',
+    voca: [],
+    vocaCount: 0,
+  },
+  {
+    userId: 2,
+    email: 'dumdum2@naver.com',
+    name: '마이크재리슨',
+    password: '222222',
+    voca: [],
+    vocaCount: 0,
+  },
+  {
+    userId: 3,
+    email: 'dumdum3@naver.com',
+    name: '긔여니',
+    password: '333333',
+    voca: [],
+    vocaCount: 0,
+  },
+];
+
+module.exports = usersInfo;
