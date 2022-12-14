5244ead7aa0d6f0e40ad426dc2051fe2e221ad4f
leejaelll <jayaom91076@gmail.com>
2022-12-14 20:23:58 +0900

    fix: Modify boxicon
    - boxicon svg 대신 i태그로 수정

--- front/src/component/VocaItem.js
+++ front/src/component/VocaItem.js
@@ -4,15 +4,15 @@ import { vocaItem, title, description, remove } from '../css/VocaItem.module.css
 class VocaItem extends Component {
   render() {
     // prettier-ignore
-    // TODO: a 태그 내의 url은 이후 수정 필요
+    // TODO: a 태그 내의 url은 이후 수정 필요 && textContent 수정 필요
     return `
       <li class="${vocaItem}">
         <a href="/wordlist"> 
           <h3 class="${title}">개발용어</h3>
           <p class="${description}">개발 용어만 모아둔 단어장. 세계 최고의 개발자가 될거야</p>
         </a>
-        <button class="${remove}">
-          <box-icon type='solid' name='trash'></box-icon>
+        <button class="${remove}" >
+          <i class='bx bxs-trash'></i>
         </button>
       </li>`;
   }
