813bd242838dca8cf2dacb58db7858c9f9abc4d9
Seonu <seonup95@gmail.com>
2022-12-14 12:15:05 +0900

    docs: Change comment about selector property
    - selector 프로퍼티에 관한 설명을 이벤트 호출자에서 이벤트 target으로 변경

--- front/src/dom/eventHolder.js
+++ front/src/dom/eventHolder.js
@@ -1,7 +1,7 @@
 /**
  * 이벤트 핸들러가 중복 등록되지 않도록 등록할 이벤트 핸들러에 관련된 정보를 객체로 받아서 eventHolder 배열에 저장함.
  * - type: 이벤트 타입
- * - selector: 이벤트 호출자
+ * - selector: 이벤트 target
  * - handler: 이벤트 핸들러
  * 추후 중복된 이벤트가 있는지 체크하는 로직 구현 (Component.js -> holdEvent 메서드)
  * @type { [{type: string, selector: string, handler: e => void}] }
