88b05f65d30bf9b4851414c6d70ea76452887128
NamgungJongMin <jmnamgung@gmail.com>
2022-12-14 15:40:57 +0900

    refactor: Modify variables name & add example data
    - 변수명 변경
    - id 형식: Date.parse(new Date())
    - userInfo 내 첫 유저 데이터에 voca 데이터 추가
    (vocaId, title, vocaDescription, words:[])

--- back/Database.js
+++ back/Database.js
@@ -1,14 +1,49 @@
+// userId와 wordId는 생성된 시간까지의 밀리초를 저장.
+// Date.parse(new Date()) 형식.
+
 const usersInfo = [
   {
-    userId: 1,
+    userId: 0,
     email: 'dumdum1@naver.com',
     name: '코카콜라맛있다',
     password: '111111',
-    voca: [],
-    vocaCount: 0,
+    voca: [
+      {
+        vocaId: '1670999143000',
+        title: '토익 영단어 모음',
+        vocaDescription: '12월 토익 800점을 위한 단어 모음집!',
+        words: [
+          { wordId: '1670999151000', word: 'retrospect', wordDescription: '회고' },
+          { wordId: '1670999191000', word: 'mother', wordDescription: '어머니' },
+          { wordId: '1670999191000', word: 'father', wordDescription: '아버지' },
+          { wordId: '1670999191000', word: 'one', wordDescription: '1' },
+          { wordId: '1670999191000', word: 'two', wordDescription: '2' },
+          { wordId: '1670999191000', word: 'three', wordDescription: '3' },
+          { wordId: '1670999191000', word: 'four', wordDescription: '4' },
+          { wordId: '1670999191000', word: 'five', wordDescription: '5' },
+          { wordId: '1670999191000', word: 'six', wordDescription: '6' },
+          { wordId: '1670999191000', word: 'seven', wordDescription: '7' },
+          { wordId: '1670999191000', word: 'eight', wordDescription: '8' },
+          { wordId: '1670999191000', word: 'nine', wordDescription: '9' },
+          { wordId: '1670999191000', word: 'ten', wordDescription: '10' },
+          { wordId: '1670999191000', word: 'eleven', wordDescription: '11' },
+        ],
+      },
+      {
+        vocaId: '1670999351000',
+        title: '경선식 영어 암기!',
+        vocaDescription: '가볍게 자주 읽을 수 있는 단어들 모음',
+        words: [
+          { wordId: '1670999151000', word: 'retrospect', wordDescription: '회고' },
+          { wordId: '1670999191000', word: 'mother', wordDescription: '어머니' },
+          { wordId: '1670999191000', word: 'father', wordDescription: '아버지' },
+        ],
+      },
+    ],
+    vocaCount: 17,
   },
   {
-    userId: 2,
+    userId: 1,
     email: 'dumdum2@naver.com',
     name: '마이크재리슨',
     password: '222222',
@@ -16,7 +51,7 @@ const usersInfo = [
     vocaCount: 0,
   },
   {
-    userId: 3,
+    userId: 2,
     email: 'dumdum3@naver.com',
     name: '긔여니',
     password: '333333',
