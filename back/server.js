9114a93cc0cb41d292edf55304293b9da103629a
NamgungJongMin <jmnamgung@gmail.com>
2022-12-16 11:00:29 +0900

    conf: Set configuration about vite & Add middleware
    - 빌드 시 컴파일 옵션 추가
    - main.js에 테스트용 axios 요청 추가
    - vite dev용 포트 9000 / preview용 포트 9090 설정
    - proxy 설정 '/api': 'http://localhost:8000'
    - server에 cors 미들웨어 추가 밑 cross-origin-allow 설정

--- back/server.js
+++ back/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const { signIn, signInSuccess } = require('./controller/auth');
+const cors = require('cors');
+const { signIn } = require('./controller/auth');
 
 const app = express();
 dotenv.config();
@@ -14,10 +15,16 @@ app.use(express.json());
 // 쿠키를 사용해 jwt를 사용할 것이기 때문에 cookie-parser 설정
 app.use(cookieParser());
 
+app.use(cors({ origin: '*', methods: ['GET', 'POST'], credentials: true }));
+
 app.post('/signIn', signIn);
 // signin이 성공했을 때 사용자가 요청을 하면 현재 쿠키에 담겨있는 AccessToken을 가지고 사용자 정보를 parsing하여 전달해주는 역할
-app.get('/signIn/success', signInSuccess);
+// app.get('/signIn/success', signInSuccess);
 // logout을 하면 현재 쿠키에 담겨져 있는 AccessToken을 제거하는 역할
+
+app.get('/', (req, res) => {
+  res.send([1, 2, 3, 4]);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is on ${process.env.PORT}`);
