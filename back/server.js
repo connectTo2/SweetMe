b125a7e5df20899d5fed5a5c3d876451b912d763
NamgungJongMin <jmnamgung@gmail.com>
2022-12-16 11:57:57 +0900

    feat: Create route template
    - 서버 요청에 필요한 라우트 템플릿 작성

--- back/server.js
+++ back/server.js
@@ -1,30 +1,45 @@
+/* --------------------------------- require -------------------------------- */
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const { signIn } = require('./controller/auth');
+const { signIn } = require('./handler/signin');
+const { logOut } = require('./handler/logout');
+const { addUser } = require('./handler/signup');
+const { getAllWords } = require('./handler/game');
+const { getVoca, addVoca, removeVoca } = require('./handler/vocalist');
+const { getWords, addWord, editWord, removeWord } = require('./handler/wordlist');
 
+/* ------------------------------------ use middleware ----------------------------------- */
 const app = express();
-dotenv.config();
-
-app.use(express.static('public'));
-
 // <클라이언트에서 서버로 요청이 가능하도록 하는 기본 설정>
 // client와 server간의 통신을 위해 JSON 형식을 다룰거기 때문에 JSON 미들웨어 설치
 app.use(express.json());
 // 쿠키를 사용해 jwt를 사용할 것이기 때문에 cookie-parser 설정
 app.use(cookieParser());
+// 클라이언트 서버 간 cors 오류 방지 (개발 단에서 임시로 origin:* 설정)
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PATCH', 'DELETE'], credentials: true }));
 
-app.use(cors({ origin: '*', methods: ['GET', 'POST'], credentials: true }));
+dotenv.config();
 
-app.post('/signIn', signIn);
-// signin이 성공했을 때 사용자가 요청을 하면 현재 쿠키에 담겨있는 AccessToken을 가지고 사용자 정보를 parsing하여 전달해주는 역할
-// app.get('/signIn/success', signInSuccess);
-// logout을 하면 현재 쿠키에 담겨져 있는 AccessToken을 제거하는 역할
+/* -------------------------------- route -------------------------------- */
+// 로그인 JWT 토큰 발행
+app.route('/signin').post(signIn);
 
-app.get('/', (req, res) => {
-  res.send([1, 2, 3, 4]);
-});
+// 로그아웃 JWT 토큰 삭제
+app.route('/logout').get(logOut);
+
+// 회원가입 서버에 유저 정보 추가
+app.route('/signup').post(addUser);
+
+// vocalist route
+app.route('/vocalist').get(getVoca).post(addVoca).delete(removeVoca);
+
+// wordlist route
+app.route('/wordlist').get(getWords).post(addWord).patch(editWord).delete(removeWord);
+
+// game route
+app.route('/game').get(getAllWords);
 
 app.listen(process.env.PORT, () => {
   console.log(`server is on ${process.env.PORT}`);
