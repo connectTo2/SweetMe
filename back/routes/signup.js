71836f2f2d6823eeaa3b7d2c2511e77e3dd2ed19
Seonu <seonup95@gmail.com>
2022-12-18 19:36:23 +0900

    feat: Create signup post api
    client에서 signup post 요청시 회원가입 로직 구현
    - 이미 가입된 유저일 경우 응답 코드 409 출력
    - 회원가입 성공시 usersInfo에 추가 및 응답코드 201 출력

--- back/routes/signup.js
+++ back/routes/signup.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const usersInfo = require('../user');
 
 const router = express.Router();
 
@@ -6,8 +7,28 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/', () => {});
+const generateNextId = () => Math.max(0, ...usersInfo.map(({ userId }) => +userId)) + 1;
 
-router.post('/', () => {});
+router.post('/', (req, res) => {
+  const { email, userName, password } = req.body;
+
+  const userInfo = usersInfo.find(user => user.email === email);
+
+  // userInfo가 있으면 이미 가입된 회원이라는 에러를 띄워줌
+  if (userInfo) {
+    res.status(409).json('Signup comflict!');
+  } else {
+    usersInfo.push({
+      userId: generateNextId(),
+      email,
+      name: userName,
+      password,
+      voca: [],
+      wordsCount: 0,
+    });
+    res.status(201).json('Signup success!');
+    console.log('[newUser]', usersInfo.at(-1));
+  }
+});
 
 module.exports = router;
