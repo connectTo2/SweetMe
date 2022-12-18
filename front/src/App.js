0f23d4c47b716fe1d226e1660944d9fa4e6ea4b7
Seonu <seonup95@gmail.com>
2022-12-18 16:01:21 +0900

    temp: Create test api
    데이터 통신 에러를 확인하기 위한 temp 파일 업로드

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,4 @@
+import render from './dom/render';
 import Component from './core/Component';
 import SignInPage from './page/SignInPage';
 import SignUpPage from './page/SignUpPage';
@@ -24,13 +25,23 @@ class App extends Component {
     this.path = '/signin';
   }
 
+  /**
+   * @type { (newPath: string) => void }
+   * 현재 path를 상태로 관리하여, 하위 컴포넌트들이 path를 변경할 수 있도록 path와 changePath를 props로 전달한다.
+   * changePath가 호출되며 새로운 path를 인수로 전달 받으면, App이 가지고 있는 path 프로퍼티 값이 변경되고 주소창의 url이 변경된 후 리렌더링이 일어난다.
+   * TODO: 상태로 관리되는 것은 state라고 명시해주는 것이 좋다고 했다. this.path 또한 state로 명시해주는게 좋지 않을까?
+   */
   changePath(newPath) {
     this.path = newPath;
+    window.history.pushState(null, null, this.path);
+    render();
   }
 
   /** 현재 path를 확인해서 routes 객체에 담긴 컴포넌트의 render 메서드를 호출하고, 반환받은 domString을 모아서 반환하는 함수 */
   render() {
+    console.log('[path]', this.path);
     const { page } = this.routes.find(({ path }) => path.test(this.path));
+    console.log('[page]', page);
 
     /**
      * 서버에 저장되어 있는 단어장(Wordlist)의 경우, vocaId를 사용하기 때문에 /WordList url에 path에 vocaId가 path에 추가되어 들어오게 된다.
@@ -38,8 +49,8 @@ class App extends Component {
      */
     const vocaId = this.path.match(/^[0-9]+$/);
     const url = vocaId
-      ? { path: this.path, vocaId, changePath: this.changePath }
-      : { path: this.path, changePath: this.changePath };
+      ? { path: this.path, vocaId, changePath: this.changePath.bind(this) }
+      : { path: this.path, changePath: this.changePath.bind(this) };
 
     /**
      * new page()는 promise를 반환한다. 따라서 promise가 successful된 결과인 pageInstance의 render 메서드를 호출하기 위해서는 두 가지 방법이 있다.
