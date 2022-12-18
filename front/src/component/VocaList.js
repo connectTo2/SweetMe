90565d1b0b4ea7ff8b6e35aebc79196ce669c71c
leejaelll <jayaom91076@gmail.com>
2022-12-19 00:47:58 +0900

    feat: Create state variable in client
    - 클라이언트에서 상태로 관리할 removeKeyword, removeVocaId 변수 생성
    - 클릭이벤트 핸들러 confirmModal이 발생하면 removeKeyword의 값을 변경하고 setState 메서드를 실행한다.
    - 클릭이벤트 핸들러 removeVocaItem가 실행되면 delete 메서드를 요청한 후 받은 데이터로 setState를 실행한다.

--- front/src/component/VocaList.js
+++ front/src/component/VocaList.js
@@ -1,25 +1,24 @@
 import Component from '../core/Component';
 import { vocaList, link, plusItem, plusIcon } from '../css/VocaList.module.css';
+
+/* -------------------------------- component ------------------------------- */
 import { VocaItem, remove } from './VocaItem';
 
 class VocaList extends Component {
   render() {
-    console.log(this.props);
-    const { routeDetailPage, vocaInfo } = this.props;
-
-    // 받은 데이터를 통해 VocaItem 컴포넌트에게 전달해주어야한다. (voca.title, voca.vocaDescription)
+    console.log('[LIST]', this.props);
+    const { routeDetailPage, voca } = this.props;
 
     // prettier-ignore
     return `
       <ul class="${vocaList}">
-        ${vocaInfo.map(vocaInfo => new VocaItem({...this.state, routeDetailPage, vocaInfo}).render()).join('')}
+        ${voca.map(vocaInfo => new VocaItem({...this.props, routeDetailPage,vocaInfo}).render()).join('')}
         <li class="${plusItem}">
           <a href="/" class="${link}">
             <i class="bx bx-plus ${plusIcon}"></i>
           </a>
         </li>
-      </ul>
-      `
+      </ul>`
   }
 
   addEventListener() {
