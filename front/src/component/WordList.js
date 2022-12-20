cb357ed2d0cac24a9619433a0a8c359715155fc9
Seonu <seonup95@gmail.com>
2022-12-20 13:00:19 +0900

    feat: Create wordlist markup & style

--- front/src/component/WordList.js
+++ front/src/component/WordList.js
@@ -1,1 +1,46 @@
-// WordList.js
+import { title, description, outlineButton } from '../css/common.module.css';
+import { input, buttonWrapper, addWord, addIcon } from '../css/WordList.module.css';
+import WordItem from './WordItem';
+
+class WordList {
+  render() {
+    return `
+      <header>
+        <h2>
+          <input
+            type="text"
+            name="title"
+            class="${input} ${title}"
+            placeholder="단어장 제목 입력"
+          />
+        </h2>
+        <input
+          type="text"
+          name="description"
+          class="${input} ${description}"
+          placeholder="단어장에 대한 설명 입력"
+        />
+      </header>
+
+      <div class="content">
+        <div class="${buttonWrapper}">
+          <button type="button" class="${outlineButton} active full">전체</button>
+          <button type="button" class="${outlineButton} wordOnly">단어만</button>
+          <button type="button" class="${outlineButton} descriptionOnly">
+            뜻만
+          </button>
+        </div>
+
+        <ul class="wordList">
+          ${new WordItem().render()}
+        </ul>
+
+        <button class="${addWord}">
+          <i class='bx bx-plus ${addIcon}'></i>
+        </button>
+      </div>
+    `;
+  }
+}
+
+export default WordList;
