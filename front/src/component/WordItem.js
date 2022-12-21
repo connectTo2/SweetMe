44b29781a4e86ce19e7043018b6c8c0ed7765a16
Seonu <seonup95@gmail.com>
2022-12-22 04:51:09 +0900

    feat: Working in progress (WordListPage)
    - WordListPage 마크업 및 스타일링 완성
    - WordListPage 기능 구현 중

--- front/src/component/WordItem.js
+++ front/src/component/WordItem.js
@@ -1,3 +1,37 @@
-class WordItem {}
+import { wordItem, input, wordInput, removeIcon } from '../css/WordList.module.css';
+import Component from '../core/Component';
+
+class WordItem extends Component {
+  render() {
+    const { wordId, word, wordDescription } = this.props;
+
+    return `
+      <li class="${wordItem}" data-id="${wordId}">
+        <input
+          type="text"
+          name="word"
+          class="${input} ${wordInput}"
+          placeholder="단어 입력"
+          value="${word ?? ''}"
+        />
+        <input
+          type="text"
+          name="wordDescription"
+          class="${input} wordDescriptionInput"
+          placeholder="뜻 입력"
+          value="${wordDescription ?? ''}"
+        />
+        <button type="button" class="removeWord">
+          <i class="bx bx-x ${removeIcon}"></i>
+        </button>
+      </li>
+    `;
+  }
+
+  addEventListener() {
+    const { removeWordList } = this.props;
+    return [{ type: 'click', selector: `.removeWord`, handler: removeWordList }];
+  }
+}
 
 export default WordItem;
--- front/src/component/WordList.js
+++ front/src/component/WordList.js
@@ -1,9 +1,14 @@
 import { title, description, outlineButton } from '../css/common.module.css';
-import { input, buttonWrapper, addWord, addIcon } from '../css/WordList.module.css';
+import { input, wordList, buttonWrapper, addWord, addIcon } from '../css/WordList.module.css';
+import Component from '../core/Component';
 import WordItem from './WordItem';
 
-class WordList {
+class WordList extends Component {
   render() {
+    const vocaTitle = this.props.title;
+    const { vocaDescription, words, removeWordList } = this.props;
+
+    // prettier-ignore
     return `
       <header>
         <h2>
@@ -12,27 +17,32 @@ class WordList {
             name="title"
             class="${input} ${title}"
             placeholder="단어장 제목 입력"
+            value="${vocaTitle ?? ''}"
           />
         </h2>
         <input
           type="text"
           name="description"
           class="${input} ${description}"
-          placeholder="단어장에 대한 설명 입력"
+          placeholder="단어장 설명 입력"
+          value="${vocaDescription ?? ''}"
         />
       </header>
 
       <div class="content">
         <div class="${buttonWrapper}">
-          <button type="button" class="${outlineButton} active full">전체</button>
+          <button type="button" class="${outlineButton} full active">전체</button>
           <button type="button" class="${outlineButton} wordOnly">단어만</button>
           <button type="button" class="${outlineButton} descriptionOnly">
             뜻만
           </button>
         </div>
 
-        <ul class="wordList">
-          ${new WordItem().render()}
+        <ul class="${wordList}">
+          ${words.map(word => new WordItem({
+              ...word, 
+              removeWordList
+            }).render()).join('')}
         </ul>
 
         <button class="${addWord}">
@@ -40,6 +50,14 @@ class WordList {
         </button>
       </div>
     `;
+  }
+
+  addEventListener() {
+    const { patchWordList, addWordList } = this.props;
+    return [
+      { type: 'input', selector: `.${input}`, handler: patchWordList },
+      { type: 'click', selector: `.${addWord}`, handler: addWordList },
+    ];
   }
 }
 
