8a7f54d653a256efb109ceb2839059ef51946b8e
NamgungJongMin <jmnamgung@gmail.com>
2022-12-22 14:40:07 +0900

    feat: Add toaster components in Signin && Signup
    - signin signup 페이지에 토스터 추가
    - 현재 두 페이지 간 상태공유 의심
    TODO: 에러해결

--- front/src/page/SignUpPage.js
+++ front/src/page/SignUpPage.js
@@ -4,22 +4,39 @@ import _ from 'lodash';
 import Component from '../core/Component';
 import { SignUp, hide, submitButton } from '../component/SignUp';
 import { signupSchema } from '../validation/schema';
+import Toaster from '../component/Toaster';
+
+let isToastShowing = false;
+let isResultSuccess = false;
 
 class SignUpPage extends Component {
   constructor(props) {
     super(props);
 
     this.changePath = this.props.changePath;
+    this.state = { isToastShowing, isResultSuccess };
   }
 
   render() {
     const signUp = new SignUp({
       valid: _.debounce(this.valid.bind(this), 200),
       postSignUp: this.postSignUp.bind(this),
-      changePath: this.changePath,
+      changePath: this.changePath.bind(this),
     }).render();
 
-    return `${signUp}`;
+    const toaster = new Toaster({
+      ...this.state,
+      type: `${this.state.result}`,
+      message: this.state.isResultSuccess ? '회원가입이 성공했습니다.' : '이미 사용중인 이메일 입니다.',
+      changePath: this.changePath,
+      pageUrl: this.state.isResultSuccess ? '/signin' : null,
+      changeIsToasterShowing: this.changeIsToasterShowing.bind(this),
+    }).render();
+
+    return `
+      ${signUp}
+      ${this.state.isToastShowing ? toaster : ''}
+      `;
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -46,6 +63,12 @@ class SignUpPage extends Component {
     this.toggleSubmitButton();
   }
 
+  changeIsToasterShowing(result) {
+    isResultSuccess = result === 'success';
+    isToastShowing = !isToastShowing;
+    this.setState({ isToastShowing });
+  }
+
   /**
    * submit 이벤트 발생시 서버에서 이미 회원가입된 유저인지 확인
    * - 중복된 이메일이면 fail toaster 띄우기
@@ -64,8 +87,9 @@ class SignUpPage extends Component {
         { email, userName, password }
       );
       console.log(`[Toaster] ${userName}님의 회원가입이 완료되었습니다!`);
-      // TODO: this.changePath('/signin');
+      this.changeIsToasterShowing('success');
     } catch (error) {
+      this.changeIsToasterShowing();
       console.log('[Toaster] 이미 가입된 회원입니다');
     }
   }
